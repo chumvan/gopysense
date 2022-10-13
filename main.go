61b99f1c0ed2857bfe50b166b562362540d32abc
Trung Van <trung.van@ericsson.com>
2022-10-13 09:45:10 +0300

    check return type of unmarshaled

--- main.go
+++ main.go
@@ -3,12 +3,24 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"log"
 
 	"github.com/chumvan/gopysense/pkg/sensehat"
 )
 
+type Measurement struct {
+	Temperature float32 `json:"temperature"`
+	Humidity    float32 `json:"humidity"`
+	Pressure    float32 `json:"pressure"`
+}
+
 func main() {
-	m := sensehat.GetAllMeasurements()
-	result, _ := json.Marshal(m)
-	fmt.Println(string(result))
+	out := sensehat.GetAllMeasurements()
+	var result Measurement
+	err := json.Unmarshal(out, &result)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%+v", result)
+	fmt.Println(string(out))
 }
